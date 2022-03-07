389c3345da17e3a4dade24b3a8a078660f40ebb3
YT <techstack.yt@gmail.com>
2022-03-07 18:09:27 +0530

    Add font to button

--- src/components/Box.js
+++ src/components/Box.js
@@ -6,6 +6,6 @@ export const Box = ({ value, onClick }) => {
     const style = value === "X" ? "box x" : "box o";
 
     return (
-        <div className={style} onClick={onClick}>{value}</div>
+        <button className={style} onClick={onClick}>{value}</button>
     )
 }
