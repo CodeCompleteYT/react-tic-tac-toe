f13955a047535b062fe18b3d43bcc98c24684d06
YT <techstack.yt@gmail.com>
2022-03-07 18:08:28 +0530

    Change button to div

--- src/components/Box.js
+++ src/components/Box.js
@@ -6,6 +6,6 @@ export const Box = ({ value, onClick }) => {
     const style = value === "X" ? "box x" : "box o";
 
     return (
-        <button className={style} onClick={onClick}>{value}</button>
+        <div className={style} onClick={onClick}>{value}</div>
     )
 }
